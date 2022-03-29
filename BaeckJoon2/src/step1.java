167c3dd0a6d1d72de14900c3a580610f5dd9da1d
HwiJae Lee <zzangka1122@gmail.com>
2022-03-29 10:34:02 +0900

    두 수 비교하기

--- BaeckJoon2/src/step1.java
+++ BaeckJoon2/src/step1.java
@@ -1,7 +1,13 @@
 import java.util.Scanner;
 
 public class step1 {
-
+		/*
+		 if(조건문) {
+		 	조건 만족시 실행문
+		 	} else {
+		 	불만족시 실행문
+		 	}
+		 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
@@ -19,5 +25,6 @@ public class step1 {
 		
 	}
 	}
+
 
 
