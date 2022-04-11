25ea305da0b310504f7aaf385806b6f965c29119
InDEF <zzangka1122@gmail.com>
2022-04-11 14:02:29 +0900

    기찍 N

---
+++ BaeckJoon3/src/step6.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class step6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for (int i = 1; i <= N ; N--) {
+			System.out.println(N);
+		}
+	}
+
+}
